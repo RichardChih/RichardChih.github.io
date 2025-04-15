48ad5b0557a0b1e2959169ef0678ec968f8e19ab
叶潇奇 <yip@xiexiaoqideMacBook-Pro.local>
2025-04-15 20:16:45 +0800

    Site updated: 2025-04-15 20:16:45

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/06/Hello-World-0/","2025/04/06/hello-world/"];function toRandomPost(){
+var posts=["2025/04/06/hello-world/","2025/04/06/Hello-World-0/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
