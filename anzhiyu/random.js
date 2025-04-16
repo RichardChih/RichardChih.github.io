4adc14949ce4bfcf212d967fcad795857e96ecf5
叶潇奇 <yip@xiexiaoqideMacBook-Pro.local>
2025-04-16 19:38:22 +0800

    Site updated: 2025-04-16 19:38:22

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/06/Hello-World-0/","2025/04/06/hello-world/","2025/04/15/我的第一篇文章/"];function toRandomPost(){
+var posts=["2025/04/06/Hexo的一些操作/","2025/04/16/Tarjan算法-SCC-LCA/","2025/04/06/hello-world/","2025/04/15/划分型DP与区间DP/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
