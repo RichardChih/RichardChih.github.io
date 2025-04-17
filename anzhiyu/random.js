fafa899f9bffdeafe71eb4316daee39b4ce60b11
叶潇奇 <yip@xiexiaoqideMacBook-Pro.local>
2025-04-17 16:27:57 +0800

    Site updated: 2025-04-17 16:27:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/06/Hexo的一些操作/","2025/04/16/Tarjan算法-SCC-LCA/","2025/04/06/hello-world/","2025/04/15/划分型DP与区间DP/","2025/04/17/文章模版/","2025/04/17/树形DP/"];function toRandomPost(){
+var posts=["2025/04/16/Tarjan算法-SCC-LCA/","2025/04/15/划分型DP与区间DP/","2025/04/06/hello-world/","2025/04/06/Hexo的一些操作/","2025/04/17/树形DP/","2025/04/17/文章模版/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
